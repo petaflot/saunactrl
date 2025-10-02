9b0e4c17c7eb892257538003d52186e03300b775
JCZD <jczd@engrenage.ch>
2025-10-02 21:57:24 +0200

    json stuff moved out of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -106,9 +106,6 @@ RelayModes relayModes[RELAY_COUNT] = {};
 enum RelayStates { RELAY_IS_OFF, RELAY_IS_ON, SOMETHING_IS_BROKEN };
 RelayStates relayStates[RELAY_COUNT] = {};  // TODO only use ifdef ELECTROMECHANICAL
 RelayStates lastRelayStates[RELAY_COUNT] = {};
-#ifdef STAGED_SSRs
-unsigned long relayDutyCycles[RELAY_COUNT] = {};
-#endif
 
 #ifdef ELECTROMECHANICAL
   #ifdef STAGED_SSRs
@@ -119,12 +116,16 @@ unsigned long relayDutyCycles[RELAY_COUNT] = {};
     #endif
   #endif
 #else
+  unsigned long relayDutyCycles[RELAY_COUNT] = {};
   #ifdef STAGED_SSRs
     #ifdef SMOOTH_TRIAC
       "only one of ELECTROMECHANICAL, STAGED_SSRs or SMOOTH_TRIAC may be enabled"
     #endif
   #endif
 #endif
+
+#include <json.cpp>
+JsonBuilder jb;
 
 /*
 void saveValueToEEPROM(int save_where, double val) {
@@ -146,79 +147,6 @@ void loadRelayModes() {
   EEPROM.get(ADDR_RELAYMODES, val);
   // TODO
 }
-
-// TODO: more precision in floats! (at least 2 decimals, possibly configurable)
-// TODO: return true/false instead of 1.000 or 0.000 when applicable
-class JsonBuilder {
-public:
-  JsonBuilder() { clear(); }
-
-  void clear() {
-    pos = snprintf(buffer, sizeof(buffer), "{");
-    first = true;
-  }
-
-  // --- Single numeric or enum ---
-  template <typename T>
-  typename std::enable_if<std::is_arithmetic<T>::value || std::is_enum<T>::value>::type
-  addValue(const char *key, T value) {
-    if (!first) pos += snprintf(buffer + pos, sizeof(buffer) - pos, ",");
-    pos += snprintf(buffer + pos, sizeof(buffer) - pos,
-                    "\"%s\":%.3f", key, static_cast<double>(value));
-    first = false;
-  }
-
-  // --- Single string ---
-  void addValue(const char *key, const char *value) {
-    if (!first) pos += snprintf(buffer + pos, sizeof(buffer) - pos, ",");
-    pos += snprintf(buffer + pos, sizeof(buffer) - pos,
-                    "\"%s\":\"%s\"", key, value);
-    first = false;
-  }
-  void addValue(const char *key, const String &value) { addValue(key, value.c_str()); }
-
-  // --- Array of numeric/enums ---
-  template <typename T, size_t N>
-  void addValue(const char *key, T (&arr)[N]) {
-    if (!first) pos += snprintf(buffer + pos, sizeof(buffer) - pos, ",");
-    pos += snprintf(buffer + pos, sizeof(buffer) - pos, "\"%s\":[", key);
-    for (size_t i = 0; i < N; i++) {
-      pos += snprintf(buffer + pos, sizeof(buffer) - pos,
-                      (i < N - 1) ? "%d," : "%d", static_cast<int>(arr[i]));
-    }
-    pos += snprintf(buffer + pos, sizeof(buffer) - pos, "]");
-    first = false;
-  }
-
-  // --- Array of floats from function pointer ---
-  void addValue(const char *key, size_t count, float (*func)(size_t)) {
-    if (!first) pos += snprintf(buffer + pos, sizeof(buffer) - pos, ",");
-    pos += snprintf(buffer + pos, sizeof(buffer) - pos, "\"%s\":[", key);
-    for (size_t i = 0; i < count; i++) {
-      float val = func(i);
-      pos += snprintf(buffer + pos, sizeof(buffer) - pos,
-                      (i < count - 1) ? "%.2f," : "%.2f", val);
-    }
-    pos += snprintf(buffer + pos, sizeof(buffer) - pos, "]");
-    first = false;
-  }
-
-  const char* finish() {
-    snprintf(buffer + pos, sizeof(buffer) - pos, "}");
-    return buffer;
-  }
-
-  bool hasValues() const {
-    return !first;  // false if nothing added yet
-  }
-
-private:
-  char buffer[256];
-  size_t pos;
-  bool first;
-};
-
-JsonBuilder jb;
 
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len, AsyncWebSocketClient *client) {
